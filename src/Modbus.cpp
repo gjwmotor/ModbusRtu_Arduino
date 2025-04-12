2c0d4a0edcb60d0a8bd35bbfcf849ee14cafe2ce
ftservo <117692731@qq.com>
2025-04-12 09:58:20 +0800

    src

---
+++ src/Modbus.cpp
@@ -0,0 +1,86 @@
+/*
+ * Modbus.h
+ * ModbusRtu硬件接口层程序
+ * 日期: 2025.04.09
+ * 作者: txl
+ */
+
+
+#include "Modbus.h"
+
+Modbus::Modbus()
+{
+	IOTimeOut = 50;
+	pSerial = NULL;
+	txLen = 0;
+}
+
+void Modbus::begin(HardwareSerial *_pSerial, long u32speed)
+{
+	pSerial = _pSerial;
+	pSerial->begin(u32speed);
+	txEnd_T35 = ((1000000*35)/u32speed+10);
+}
+
+void Modbus::begin(HardwareSerial *_pSerial, long u32speed, uint32_t config, int8_t rxPin, int8_t txPin)
+{
+	pSerial = _pSerial;
+	pSerial->begin(u32speed, config, rxPin, txPin);
+	txEnd_T35 = ((1000000*35)/u32speed+10);
+}
+
+int Modbus::readModbus(uint8_t *nDat, uint8_t nLen)
+{
+	int Size = 0;
+	int ComData;
+	unsigned long t_begin = millis();
+	unsigned long t_user;
+	while(1){
+		ComData = pSerial->read();
+		if(ComData!=-1){
+			if(nDat){
+				nDat[Size] = ComData;
+			}
+			Size++;
+			t_begin = millis();
+		}
+		if(Size>=nLen){
+			break;
+		}
+		t_user = millis() - t_begin;
+		if(t_user>IOTimeOut){
+			break;
+		}
+	}
+	return Size;
+}
+
+int Modbus::writeModbus(uint8_t *nDat, uint8_t nLen)
+{
+	if(nDat==NULL){
+		return 0;
+	}
+	if(isFlush){
+		return pSerial->write(nDat, nLen);
+	}else{
+		memcpy(txBuf+txLen, nDat, nLen);
+		txLen += nLen;
+		return txLen;
+	}
+}
+
+
+void Modbus::flushModbusRx()
+{
+	while(pSerial->read()!=-1);
+}
+
+void Modbus::flushModbusTx()
+{
+	if(!isFlush){
+		pSerial->write(txBuf, txLen);
+		txLen = 0;
+	}
+	pSerial->flush();
+	delayMicroseconds(txEnd_T35);
+}
---
+++ src/Modbus.h
@@ -0,0 +1,36 @@
+﻿/*
+ * Modbus.h
+ * ModbusRtu硬件接口层程序
+ * 日期: 2025.04.09
+ * 作者: txl
+ */
+
+#ifndef _MODBUS_H
+#define _MODBUS_H
+
+#include "Arduino.h"
+#include "Rtu.h"
+
+
+class Modbus : public Rtu
+{
+public:
+	Modbus();
+	void begin(HardwareSerial *_pSerial, long u32speed);
+	void begin(HardwareSerial *_pSerial, long u32speed, uint32_t config, int8_t rxPin, int8_t txPin);
+	void setTimeOut(uint16_t u16timeOut) { IOTimeOut = u16timeOut; }
+protected:
+	virtual int writeModbus(uint8_t *nDat, uint8_t nLen);//输出nLen字节
+	virtual int readModbus(uint8_t *nDat, uint8_t nLen);//输入nLen字节
+	virtual void flushModbusRx();//刷新接收缓冲区
+	virtual void flushModbusTx();//刷新发送缓冲区
+	
+public:
+	uint32_t IOTimeOut;//输入输出超时
+	HardwareSerial *pSerial;//串口指针
+    uint32_t txEnd_T35;
+	uint8_t txBuf[256];
+	uint8_t txLen;
+};
+
+#endif
